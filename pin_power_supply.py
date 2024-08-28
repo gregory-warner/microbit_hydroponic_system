ba5bbaef7e58373541cdb89b30f1ffbad33d5065
Greg Warner <>
2024-08-27 21:58:45 -0500

    add pin power supply

--- pin_power_supply.py
+++ pin_power_supply.py
@@ -2,12 +2,15 @@ from microbit import *
 from pin_status_display import PinStatusDisplay
 from interval_timer import IntervalTimer
 
-class PinPowerSupply:
-    pin_status = {
-        'on': 1,
-        'off': 0
-    }
+class Power:
+    off=0
+    on=1
 
+class Mode:
+    power=0
+    settings=1
+
+class PinPowerSupply:
     def __init__(self):
         """
         Initializes a new instance of the PinPowerSupply class. Sets up three pins (pin0, pin1, and pin2) on the microbit board,
@@ -15,63 +18,63 @@ class PinPowerSupply:
         By default, power is turned on to pin0.
         """
         self.pins = [pin0, pin1, pin2]
-        self._pin = self.pins[0]
         self.timer = IntervalTimer()
+        self.initialize_pin()
 
-        self.update_pin_power(self.pins[0], self.pin_status['on'])
+    def initialize_pin(self):
+        pin_number = 0
+        self._pin = self.pins[pin_number]
+        self.mode = Mode.power
+        self.power_status = Power.on
+        self.update_pin(pin_number)
 
-    def pin_power_listener(self):
-        """
-        Monitors for button and touch inputs and updates the power status or interval timer settings.
-        If both buttons are pressed while a pin is touched, switch the power to that pin.
-        If both buttons are pressed without any pins being touched, change the interval duration of the timer.
-        """
+    def enable_settings_mode(self):
+        self.mode = Mode.settings
+        self.power_status = Power.off
+        self.write_pin()
+        display.scroll("Settings")
+
+    def enable_pin_touch_handler(self):
         both_buttons_pressed = button_a.is_pressed() and button_b.is_pressed()
 
-        if self.pins[0].is_touched() and both_buttons_pressed:
-            self.switch_pin(self.pins[0])
-            return
+        if both_buttons_pressed and self.pins[0].is_touched():
+            self.update_pin(0)
+        elif both_buttons_pressed and self.pins[1].is_touched():
+            self.update_pin(1)
+        elif both_buttons_pressed and self.pins[2].is_touched():
+            self.update_pin(2)
+
+    def update_pin(self, pin_number: int) -> None:
+        self.set_pin(pin_number)
+        self.power_status = Power.on
+        self.write_pin()
+        self.disable_settings_mode()
+
+    def disable_settings_mode(self):
+        self.power_status = Power.on
+        self.mode = Mode.power
         
-        if self.pins[1].is_touched() and both_buttons_pressed:
-            self.switch_pin(self.pins[1])
-            return
-        
-        if self.pins[2].is_touched() and both_buttons_pressed:
-            self.switch_pin(self.pins[2])
-            return
-        
-        if both_buttons_pressed:
-            self.timer.change_interval()
+    def set_pin(self, pin_number) -> None:
+        self._pin = self.pins[pin_number]
 
     def check_interval_timer(self):
         """
         Checks the interval timer and toggles the power status if the timer interval is complete
         """
         if self.timer.get_remaining_time() <= 0:
-        
-            self.update_pin_power(self._pin, 0)
+            self.toggle_pin_power()
             self.timer.reset()
-        
-    def switch_pin(self, pin):
-        """
-        Switches the pin that will be powered on and off. If another pin is currently powered on, turns off that pin before turning on the new one.
-        Args:
-            pin (microbit.pin): The new pin with power status on
-        """
-        if self._pin == pin:
-            return
 
-        if self._pin is not None:
-            self.update_pin_power(self._pin, self.pin_status['off'])
+    def toggle_pin_power(self):
+        current_power = self._pin.read_digital()
+        self.power_status = not current_power
+        self.write_pin()
 
-        self.update_pin_power(pin, self.pin_status['on'])
-    
-    def update_pin_power(self, pin, status):
-        """
-        Updates the power status of a specified pin and displays the current power status of the pin.
-        Args:
-            pin (microbit.pin): The pin to update the power status.
-            status (int): 0: off or 1: on.
-        """
-        pin.write_digital(status)
-        PinStatusDisplay.display_pin_status(self.pins.index(pin), status)
+    # def read_pin(self, pin=None):
+    #     if pin is None:
+    #         pin = self._pin
+    #     display.show(pin.read_digital(), delay=3000)
+
+    def write_pin(self):
+        self._pin.write_digital(self.power_status)
+        PinStatusDisplay.display_pin_status(self.pins.index(self._pin), self.power_status)
