8a42c18fdd9fe7a4c55d00a6e96c97ed72e39d32
Greg Warner <cats-carrying-canes@canary.net>
2024-08-29 21:39:50 -0500

    move button listening logic to main
    fixed unresponsive microbit

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from pin_power_supply import PinPowerSupply
+from microbit_pin_controller import *
+from microbit import *
 
-ps = PinPowerSupply()
+ps = MicrobitPinController()
+
 
 while True:
-    ps.pin_power_listener()
-    ps.check_interval_timer()
+    if button_a.is_pressed() and button_b.is_pressed():
+        ps.toggle_mode()
+        continue
+
+    if ps.mode == Mode.settings:
+        for pin in ps.pins:
+            if pin.is_touched():
+                ps.set_pin_on(pin)
+                break
+    # if ps.mode == Mode.power:
+
+            
+    # elif ps.mode == Mode.settings:
+        
+    #     if ps.is_change_interval_pressed:
+    #         ps.update_interval_timer()
+
+
+    # ps.check_interval_timer()
