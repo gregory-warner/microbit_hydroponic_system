4b41d951f31bbf79aa6149c4fc2026873b25e9b0
Greg Warner <cats-carrying-canes@canary.net>
2024-08-29 23:11:56 -0500

    add listener to loop

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 from microbit_pin_controller import *
+from microbit_listener import MicrobitListener
 from microbit import *
 
 ps = MicrobitPinController()
-
+listener = MicrobitListener(ps)
 
 while True:
-    if button_a.is_pressed() and button_b.is_pressed():
-        ps.toggle_mode()
-        continue
+    listener.on_button_press()
+    listener.on_pin_touch()
 
-    if ps.mode == Mode.settings:
-        for pin in ps.pins:
-            if pin.is_touched():
-                ps.set_pin_on(pin)
-                break
-    # if ps.mode == Mode.power:
-
-            
-    # elif ps.mode == Mode.settings:
-        
-    #     if ps.is_change_interval_pressed:
-    #         ps.update_interval_timer()
-
-
-    # ps.check_interval_timer()
+    if ps.is_timer_expired():
+        ps.timer_expired_handler()
