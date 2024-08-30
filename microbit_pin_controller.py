002a3fa88ff5020585087f8b18322a3b4eb3c954
Greg Warner <cats-carrying-canes@canary.net>
2024-08-29 21:39:23 -0500

    fix pin controller
    added a set pin on and toggle mode methode

--- microbit_pin_controller.py
+++ microbit_pin_controller.py
@@ -1,55 +1,43 @@
 from microbit import *
-from pin_status_display import PinStatusDisplay
+from status_display import StatusDisplay
 from interval_timer import IntervalTimer
 
 class Power:
-    off=0
-    on=1
+    off = 0
+    on = 1
 
 class Mode:
-    power=0
-    settings=1
+    power = 0
+    settings = 1
 
 class MicrobitPinController:
     def __init__(self):
-        """
-        Initializes a new instance of the MicrobitPinController class. Sets up three pins (pin0, pin1, and pin2) on the microbit board,
-        and initializes an interval timer to control the duration between power toggles for a selected pin.
-        By default, power is turned on to pin0.
-        """
         self.pins = [pin0, pin1, pin2]
         self.timer = IntervalTimer()
-        self.initialize_pin(0)
+        self._pin = None
+        self.set_pin_on(self.pins[0])
 
-    def initialize_pin(self, pin_index: int) -> None:
-        self._pin = self.pins[pin_index]
+        self.is_mode_change_pressed = button_a.is_pressed() and button_b.is_pressed()
+        self.is_change_interval_pressed = self.mode == Mode.settings and button_b.is_pressed()
+
+    def toggle_mode(self) -> None:
+        self.mode = not self.mode
+        if self.mode == Mode.settings:
+            StatusDisplay.display_settings()
+
+    def set_pin_on(self, pin) -> None:
+        if self._pin == pin or pin.read_digital() == Power.on:
+            return
+
+        self._pin = pin
         self.mode = Mode.power
-        self.power_status = Power.on
-        self.update_pin(pin_index)
+        self.write_pin(self._pin, Power.on)
 
-    def enable_settings_mode(self):
-        self.mode = Mode.settings
-        self.power_status = Power.off
-        [pin.write_digital(0) for pin in self.pins]
-
-    def enable_pin_touch_listener(self):
-        [pin.write_digital(0) for pin in self.pins]
-
-        if button_a.is_pressed():
-            for i, pin in enumerate(self.pins):
-                if pin.is_touched():
-                    self.update_pin(i)
-
-    def update_pin(self, pin_number: int) -> None:
-        self._pin = self.pins[pin_number]
-        self.power_status = Power.on
-        self.mode = Mode.power
-        self.write_pin()
+    def update_interval_timer(self):
+        self.timer.change_interval()
+        self.timer.reset()
 
     def check_interval_timer(self):
-        """
-        Checks the interval timer and toggles the power status if the timer interval is complete
-        """
         if self.timer.get_remaining_time() <= 0:
             self.toggle_pin_power()
             self.timer.reset()
@@ -57,8 +45,15 @@ class MicrobitPinController:
     def toggle_pin_power(self):
         current_power = self._pin.read_digital()
         self.power_status = not current_power
-        self.write_pin()
+        self.write_pin(self._pin, self.power_status)
 
-    def write_pin(self):
-        self._pin.write_digital(self.power_status)
-        PinStatusDisplay.display(self.pins.index(self._pin), self.power_status)
+    def write_pin(self, pin, status: int) -> None:
+        self.power_status = status
+        pin_index = self.pins.index(pin)
+        self.pins[pin_index].write_digital(status)
+        StatusDisplay.display_pin_status(pin_index, status)
+
+    def reset_pins(self):
+        for pin in self.pins:
+            if pin.read_digital() == Power.on:
+                self.write_pin(pin, Power.off)
