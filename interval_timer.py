2e30a64d532b9bf935400679ffc48a511f243cac
Greg Warner <cats-carrying-canes@canary.net>
2024-09-05 22:43:47 -0500

    add timer expired to interval timer

--- interval_timer.py
+++ interval_timer.py
@@ -1,4 +1,4 @@
-from microbit import running_time, display
+from microbit import running_time
 
 millis_in_second = 1000
 seconds_in_minute = 60
@@ -6,16 +6,16 @@ minute_in_hour = 60
 
 class IntervalTimer:
     def __init__(self):
-        self.start_time = running_time()
+        self.start_time = None
         # intervals in hours
         self.intervals = [1, 2, 4, 8, 16, 24]
         self.current_interval_index = 0
-        self.interval = self.hours_to_millis(self.intervals[self.current_interval_index])
+        self.interval = self._hours_to_millis(self.intervals[self.current_interval_index])
 
-    def hours_to_millis(self, hour) -> int:
+    def _hours_to_millis(self, hour) -> int:
         return hour * minute_in_hour * seconds_in_minute * millis_in_second
     
-    def millis_to_hours(self, millis) -> int:
+    def _millis_to_hours(self, millis) -> int:
         return millis // (millis_in_second * seconds_in_minute * minute_in_hour)
 
     def reset(self):
@@ -24,10 +24,10 @@ class IntervalTimer:
     def elapsed(self) -> int:
         return running_time() - self.start_time
     
-    def get_remaining_time(self) -> int:
-        return self.interval - self.elapsed()
-    
-    def change_interval(self):
+    def is_timer_expired(self) -> bool:
+        return self.start_time is not None and self.interval - self.elapsed() <= 0
+
+    def change_interval(self) -> int:
         self.current_interval_index = (self.current_interval_index + 1) % len(self.intervals)
-        self.interval = self.hours_to_millis(self.intervals[self.current_interval_index])
-        display.scroll(str(self.millis_to_hours(self.interval)))
+        self.interval = self._hours_to_millis(self.intervals[self.current_interval_index])
+        return self._millis_to_hours(self.interval)
